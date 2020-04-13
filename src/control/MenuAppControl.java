5d8d255daef808acabd38eec4b46807f1743e110
Marc_Bourbonnot <marc.bourbonnot@u-psud.fr>
2020-04-13 14:45:52 +0200

    Ajout des triangles correction aux Rectangle toujours les ellipses bug (en mieux)

--- src/control/MenuAppControl.java
+++ src/control/MenuAppControl.java
@@ -57,7 +57,6 @@ public class MenuAppControl {
             System.out.println(this.ctrl.getMdl().getTypeSelected());
             this.ctrl.getMdl().newForme();
         });
-        //Triangles
         ((Menu) ((Menu) insertion.get(0)).getItems().get(2)).getItems().get(0).setOnAction(e ->{
             this.ctrl.getMdl().setTypeSelected(Formes.TRIANGLE_ISOCELE);
             System.out.println(this.ctrl.getMdl().getTypeSelected());
@@ -68,11 +67,6 @@ public class MenuAppControl {
             System.out.println(this.ctrl.getMdl().getTypeSelected());
             this.ctrl.getMdl().newForme();
         });
-//        ((Menu)((Menu) insertion.get(0)).getItems().get(2).setOnAction(e ->{
-//            this.ctrl.getMdl().setTypeSelected(Formes.TRIANGLE);
-//            System.out.println(this.ctrl.getMdl().getTypeSelected());
-//            this.ctrl.getMdl().newForme();
-//        });
         ((Menu)insertion.get(0)).getItems().get(3).setOnAction(e -> {
             this.ctrl.getMdl().setTypeSelected(Formes.ELLIPSE);
             System.out.println(this.ctrl.getMdl().getTypeSelected());
