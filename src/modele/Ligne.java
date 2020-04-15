c073c5ef84f8f995155ded4d7792d439f4057358
jeanb <jeanbaptiste.bourgeois@live.fr>
2020-04-15 15:06:43 +0200

    refection de estDedans pour les lignes, encore un peu bugué pour les lignes trop vertical mais pas completement verticale

--- src/modele/Ligne.java
+++ src/modele/Ligne.java
@@ -5,7 +5,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Paint;
 
 public class Ligne extends Forme {
-    public final static int MARGE = 5;
+    public final static int MARGE = 20;
 
     public Ligne() {
         super();
@@ -24,8 +24,33 @@ public class Ligne extends Forme {
     }
 
     @Override
-    public boolean estDedans(double x, double y) {
+    /*public boolean estDedans(double x, double y) {
         return x >= this.getX() && x <= this.getWidth() && y >= this.getY() && y <= this.getHeight();
+    }*/
+    public boolean estDedans(double x, double y){
+        //calcul de l'equation de la droite
+        double a = (this.getHeight()-this.getY())/(this.getWidth()-this.getX());
+        double b = this.getY()-a*this.getX();
+        double btest = 0;
+
+        double bMax = b+MARGE;
+        double bMin = b-MARGE;
+
+        if(a>Double.MAX_VALUE){
+            btest = y;
+        }else{
+            btest = y - a*x;
+        }
+
+        if(x >= this.getX()-MARGE && x<=this.getWidth()-MARGE){
+            return btest >= bMin && btest <= bMax;
+        }
+        if(btest == y){
+            if(y >= this.getY() && y <= this.getHeight()){
+                return x >= this.getX()-MARGE && x <= this.getX()+MARGE;
+            }
+        }
+        return false;
     }
 
     @Override
@@ -36,6 +61,8 @@ public class Ligne extends Forme {
         gc.strokeLine(this.getX(), this.getY(), this.getWidth(), this.getHeight());
 
         gc.setFill(save);
+
+        System.out.println("a = " +(this.getHeight()-this.getY())/(this.getWidth()-this.getX()));
     }
 
     @Override
