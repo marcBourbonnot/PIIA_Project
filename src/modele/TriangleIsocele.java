5a64c056c1476fae26bfd6812ea1532b01699e0d
jeanb <jeanbaptiste.bourgeois@live.fr>
2020-04-16 21:57:19 +0200

    On peut enfin bouger les triangles !

--- src/modele/TriangleIsocele.java
+++ src/modele/TriangleIsocele.java
@@ -27,7 +27,22 @@ public class TriangleIsocele extends Forme {
     @Override
     public boolean estDedans(double x, double y) {
 
-        return false;
+        double abc = this.calculAire(this.getX(), this.getY(), this.getWidth(), this.getHeight(),this.getX() - (this.getWidth() - this.getX()), this.getHeight() );
+        double axb = this.calculAire(this.getX(), this.getY(), x, y, this.getWidth(), this.getHeight());
+        double axc = this.calculAire(this.getX(), this.getY(), x, y, this.getX() - (this.getWidth() - this.getX()), this.getHeight());
+        double bxc = this.calculAire(this.getWidth(), this.getHeight(), x, y, this.getX() - (this.getWidth() - this.getX()), this.getHeight());
+
+        System.out.println("abc "+ abc);
+        System.out.println("axb "+axb);
+        System.out.println(" axc "+axc);
+        System.out.println(" bxc "+bxc);
+        System.out.println(" somme des trois "+(axb+axc+bxc));
+
+        return abc == (axb+axc+bxc);
+    }
+
+    public double calculAire(double xA, double yA, double xB, double yB, double xC, double yC){
+        return Math.abs((xB-xA)*(yC-yA)-(xC-xA)*(yB-yA));
     }
 
     @Override
@@ -39,6 +54,7 @@ public class TriangleIsocele extends Forme {
         gc.fillPolygon(new double[]{this.getX(), this.getWidth(), this.getX() - (this.getWidth() - this.getX())}, new double[]{this.getY(), this.getHeight(), this.getHeight()},3);
         gc.strokePolygon(new double[]{this.getX(), this.getWidth(), this.getX() - (this.getWidth() - this.getX())}, new double[]{this.getY(), this.getHeight(), this.getHeight()},3);
 
+        System.out.println(this.getX());
         gc.setFill(save);
     }
 
@@ -46,4 +62,6 @@ public class TriangleIsocele extends Forme {
     public Point2D getCenter() {
         return null;
     }
+
+
 }
--- src/modele/TriangleRectangle.java
+++ src/modele/TriangleRectangle.java
@@ -26,7 +26,23 @@ public class TriangleRectangle extends Forme {
     //Methodes
     @Override
     public boolean estDedans(double x, double y) {
-        return false;
+
+        double abc = this.calculAire(this.getX(), this.getY(), this.getWidth(), this.getHeight(),this.getX() - (this.getWidth() - this.getX()), this.getHeight() );
+        double axb = this.calculAire(this.getX(), this.getY(), x, y, this.getWidth(), this.getHeight());
+        double axc = this.calculAire(this.getX(), this.getY(), x, y, this.getX() - (this.getWidth() - this.getX()), this.getHeight());
+        double bxc = this.calculAire(this.getWidth(), this.getHeight(), x, y, this.getX() - (this.getWidth() - this.getX()), this.getHeight());
+
+        System.out.println("abc "+ abc);
+        System.out.println("axb "+axb);
+        System.out.println(" axc "+axc);
+        System.out.println(" bxc "+bxc);
+        System.out.println(" somme des trois "+(axb+axc+bxc));
+
+        return abc == (axb+axc+bxc);
+    }
+
+    public double calculAire(double xA, double yA, double xB, double yB, double xC, double yC){
+        return Math.abs((xB-xA)*(yC-yA)-(xC-xA)*(yB-yA));
     }
 
     @Override
